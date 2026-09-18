b30920227ba12b9e082230c253d6916bce301df7
Matheus Giehl <giehlmatheus96@gmail.com>
2026-09-18 19:54:29 -0300

    sets

--- Python/Sets.py
+++ Python/Sets.py
@@ -17,3 +17,11 @@ if valor in meu_conjunto :
     print(f"O Valor : {valor} está no conjunto")
 else :
     print(f"O valor : {valor} não está no conjunto")
+
+#Conjunto com repetição, para estrair somente um elemento para cada valor igual.
+meu_conjunto_repetido = [1,2,3,3,3,4,4,4,4,5,5,5,5]
+
+#Variavel com set()
+teste = set(meu_conjunto_repetido)
+
+print(teste)
