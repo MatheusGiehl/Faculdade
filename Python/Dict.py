027a11d989017371c7f2cabd7ea6198a99073a0a
Matheus Giehl <giehlmatheus96@gmail.com>
2026-09-18 20:03:34 -0300

    dict use

---
+++ Python/Dict.py
@@ -0,0 +1,23 @@
+# 4 maneiras de uso 
+
+#I
+dici_1 = {}
+
+dici_1['name'] = 'Maria'
+dici_1['age'] = 25
+
+print(dici_1)
+
+#II
+dice_2 = {'name' : 'João', 'age': 28}
+
+print(dice_2)
+
+#III
+dice_3 = dict(name = 'Lucas', age = 35)
+
+print(dice_3)
+
+#IV
+dice_4 = dict(zip(['name', 'age'], ['Pedro', 33]))
+print(dice_4)
